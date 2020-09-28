00a5ccc913a18b207703408771ff46982e4a1c06
Tyler Spencewood <tspencewood@gmail.com>
2020-09-28 16:17:05 -0500

    Remove headers

--- pages/headers.js
+++ pages/headers.js
@@ -1,14 +1,6 @@
 import React from "react";
 
-export const getServerSideProps = ({ res }) => {
-  res.setHeader("Surrogate-Key", "route");
-  res.setHeader(
-    "Surrogate-Control",
-    "max-age=60, stale-while-revalidate=60, stale-if-error=300"
-  );
-  res.setHeader("Cache-Control", "must-revalidate");
-  res.setHeader("Last-Modified", new Date().toUTCString());
-
+export const getServerSideProps = () => {
   return {
     props: {},
   };
